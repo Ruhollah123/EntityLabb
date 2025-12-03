05fb222cc37846e46d625a175dbaa9d6f74327a1
Ruhollah123 <ruhollahkarim894@gmail.com>
2025-12-03 15:14:35 +0100

    courses

--- EntityLabb/Program.cs
+++ EntityLabb/Program.cs
@@ -22,23 +22,10 @@ public class Education
 public class Courses
 {
     public int Id { get; set; }
-<<<<<<< HEAD
     public string Name { get; set; }
     public string Description { get; set; }
-    // Navigation property (Many-To-One)
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
 
-}
-
-public class  Students
-{
-    public  int Id { get; set; }
-    public  string FirstName { get; set; }
-    public  string LastName { get; set; }
-=======
-    public string? CourseName  { get; set; }
-    public decimal PriceTotal { get; set; }
-    // Navigation property (Many-To-One)
-    // public Customer Customer { get; set; } = null!;
->>>>>>> 6242eb84a3d9a98913423392b68627d92a7a8f01
 }
 
