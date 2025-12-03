4e03de59f7ffeb5702ba75d58f05be6b1fd231e4
Ruhollah123 <ruhollahkarim894@gmail.com>
2025-12-03 14:46:33 +0100

    lagt in klasserna

--- EntityLabb/Program.cs
+++ EntityLabb/Program.cs
@@ -8,3 +8,21 @@
         }
     }
 }
+
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string CompanyName { get; set; } = null!;
+    public string City { get; set; } = null!;
+    // Navigation property (One-To-Many)
+    public List<Order> Orders { get; set; } = null!;
+}
+public class Order
+{
+    public int Id { get; set; }
+    public int CustomerId { get; set; }
+    public decimal PriceTotal { get; set; }
+    // Navigation property (Many-To-One)
+    public Customer Customer { get; set; } = null!;
+}
