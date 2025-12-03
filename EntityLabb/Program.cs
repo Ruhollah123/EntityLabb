e82498cf9428ddcc0e5ec1b04cfc7e6922954470
Fideli Lundgren <32635804+FideliLundgren@users.noreply.github.com>
2025-12-03 15:09:55 +0100

    Courses

--- EntityLabb/Program.cs
+++ EntityLabb/Program.cs
@@ -22,9 +22,11 @@ public class Education
 public class Courses
 {
     public int Id { get; set; }
-    public string? CourseName  { get; set; }
-    public decimal PriceTotal { get; set; }
-    // Navigation property (Many-To-One)
-    // public Customer Customer { get; set; } = null!;
+    public string CourseName { get; set; } = null!;
+    public string Describtion { get; set; } = null!;
+
+    public DateTime StartDate { get; set; } 
+    public DateTime EndDate { get; set; }
+   
 }
 
