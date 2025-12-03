556487fcbaf96c0adac7ed363267b987982bdd29
Ruhollah123 <ruhollahkarim894@gmail.com>
2025-12-03 15:34:51 +0100

    kvbj

--- EntityLabb/Program.cs
+++ EntityLabb/Program.cs
@@ -25,18 +25,21 @@ public class Courses
 {
     public int Id { get; set; }
 <<<<<<< HEAD
+<<<<<<< HEAD
     public string Name { get; set; }
     public string Description { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
 =======
+=======
+>>>>>>> e82498cf9428ddcc0e5ec1b04cfc7e6922954470
     public string CourseName { get; set; } = null!;
     public string Describtion { get; set; } = null!;
 
     public DateTime StartDate { get; set; } 
     public DateTime EndDate { get; set; }
    
->>>>>>> e82498cf9428ddcc0e5ec1b04cfc7e6922954470
+
 }
 
 
